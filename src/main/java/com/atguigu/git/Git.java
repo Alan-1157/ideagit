f0448d5759c07fa7d15f01e6042d9babc2034aa1
liuliu-f <65294549+liuliu-f@users.noreply.github.com>
2020-11-10 18:29:48 +0800

    在线更新

--- src/main/java/com/atguigu/git/Git.java
+++ src/main/java/com/atguigu/git/Git.java
@@ -7,6 +7,7 @@ public class Git {
         System.out.println("Hello git");
         System.out.println("创建分支" );
         System.out.println("主干添加" );
+        System.out.println("在线更新")；
 
     }
 }
